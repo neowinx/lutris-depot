83677b8c44979d14232b0aab8b0d7fd09d16bd16
Pedro Flores <pdavidflores@gmail.com>
2018-09-14 19:28:15 +0000

    Added some scripts to automate installer tests

---
+++ fix_config_vdf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -sL bit.ly/neowinx-config-vdf > ~/.local/share/lutris/runners/winesteam/prefix64/drive_c/Program\ Files\ \(x86\)/Steam/config/config.vdf
---
+++ reset_lutris.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf ~/.local/share/lutris/runners/winesteam
+rm -rf ~/.config/lutris/games/*
+rm -rf ~/.config/lutris/runners/
+rm -rf  ~/.local/share/lutris/pga.db
